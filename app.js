34785de2f7aa2f9fa333071a79bf2a4902a1ef1b
Harmandeep Pannu <iharmanpannu@gmail.com>
2018-06-12 16:16:24 -0700

    commit

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const gpio = require('rpi-gpio');
-const gpiop = gpio.promise;
-
+gpio.setup(9, gpio.DIR_OUT);
+gpio.setup(10, gpio.DIR_OUT);
+gpio.setup(11, gpio.DIR_OUT);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -25,10 +26,9 @@ app.use('/users', usersRouter);
 
 
 app.post('/led/on', function (req, res) {
-    gpio.write(7, true, function (err) {
+    gpio.write(11, true, function (err) {
         if (err) throw err;
-        console.log('Written True to pin');
-        console.log(path.join(__dirname, 'public'));
+        console.log('Led is now on and Green');
         return res.render('index', {
             status: "Cool!!Led is On"
         });
@@ -37,33 +37,16 @@ app.post('/led/on', function (req, res) {
 });
 
 
+app.post('/led/off', function (req, res) {
+    gpio.write(11, true, function (err) {
+        if (err) throw err;
+        console.log('Led is not off and red');
+        return res.render('index', {
+            status: "Ohh!! Led is Off"
+        });
+    });
 
-app.post('/led/on', (req, res) => {
-    gpiop.setup(11, gpio.DIR_OUT)
-        .then(() => {
-            return gpiop.write(11, true)
-        })
-        .catch((err) => {
-            console.log('Error: ', err.toString())
-        })
-    console.log('body: ' + JSON.stringify(req.body));
-    res.send(req.body)
-})
-
-
-
-// app.post('/led/on', (req, res) => {
-//     gpio.setup(7, gpio.DIR_IN, readInput);
-//     const obj = {
-//         Status: 'Light is on'
-//     }
-//     console.log('body: ' + JSON.stringify(req.body));
-//     res.send(req.body)
-// })
-// app.post('/led/off', (req, res) => {
-//     res.send('Led is now off')
-
-// })
+});
 
 
 
