ca491162622fc41ebbfcf101964461a9a26e1de8
Harmandeep Pannu <iharmanpannu@gmail.com>
2018-06-12 17:45:36 -0700

    changes

--- app.js
+++ app.js
@@ -25,16 +25,33 @@ app.use('/users', usersRouter);
 app.post('/led/on', )
 
 app.post('/led/on', function (req, res) {
-    rpio.open(11, rpio.INPUT);
+    // rpio.open(11, rpio.INPUT);
+    rpio.open(11, rpio.OUTPUT, rpio.LOW);
     console.log('Pin 11 is currently ' + (rpio.read(11) ? 'high' : 'low'));
+    for (var i = 0; i < 5; i++) {
+        /* On for 1 second */
+        rpio.write(11, rpio.HIGH);
+        rpio.sleep(1);
 
+        /* Off for half a second (500ms) */
+        rpio.write(11, rpio.LOW);
+        rpio.msleep(500);
+    }
 });
 
 
 app.post('/led/off', function (req, res) {
-    rpio.open(7, rpio.INPUT);
-    console.log('Pin 11 is currently ' + (rpio.read(7) ? 'high' : 'low'));
+    rpio.open(10, rpio.OUTPUT, rpio.LOW);
+    console.log('Pin 10 is currently ' + (rpio.read(10) ? 'high' : 'low'));
+    for (var i = 0; i < 5; i++) {
+        /* On for 1 second */
+        rpio.write(10, rpio.HIGH);
+        rpio.sleep(1);
 
+        /* Off for half a second (500ms) */
+        rpio.write(10, rpio.LOW);
+        rpio.msleep(500);
+    }
 });
 
 
